55dcd51752fc9d4e9c2c2e734f03c641c1aa213b
Daryll Herberger <daryllh@vertigo.com>
2013-06-19 14:08:11 -0700

    Added shell script handy for locating unreferenced images in an Xcode project, refining overall toolset.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -29,3 +29,6 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+echo "Installing script to detect unused images in Xcode projects..."
+chmod 755 $dir/unusedImages.sh
